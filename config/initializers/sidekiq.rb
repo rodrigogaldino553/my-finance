6c94a3c704fad057c47f3b572d87bde176d57d37
Rodrigo <galdinorodrigo553@gmail.com>
2026-02-21 02:56:45 -0300

    feat: implements serialization to election and candidates

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,18 @@
 return if Rails.env.test?
 
-Sidekiq::Cron::Job.create(
-  name: "Payment Notification - Daily",
-  cron: "0 8 * * *", # Daily at 8 AM
-  class: "PaymentNotificationJob"
-)
+# config/initializers/sidekiq.rb
+if Rails.env.production? || ENV['REDIS_URL'].present?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
+end
+
+# Sidekiq::Cron::Job.create(
+#   name: "Payment Notification - Daily",
+#   cron: "0 8 * * *", # Daily at 8 AM
+#   class: "PaymentNotificationJob"
+# )
